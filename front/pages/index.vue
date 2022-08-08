ed227deedfe2765509882d776cee4b4b93e3611f
ryosuke fukuyama <ryosukemakaritoru425@gmail.com>
2022-08-08 19:23:58 +0900

    [add] #3 親モデルで更新機能 + モーダルコンポーネント実装

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,9 +1,88 @@
 <template>
-  <Tutorial/>
+  <div>
+    <ul>
+      <li v-for="property in propertyList" :key="property.id">
+        {{ property.content }}
+        <select v-model.lazy="selected" @change="contentUpdate(property.id, $event)">
+          <option disabled value="">{{ property.content }}</option>
+          <option value="content A">"content A"</option>
+          <option value="content B">"content B"</option>
+          <option value="content C">"content C"</option>
+        </select>
+        <div  v-if="modalFlag && missId==property.id">
+          <Modal @close-click="closeModal"></Modal>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  data:() => {
+    return {
+      properties: [],
+      id: '',
+      uid: '',
+      // selected: '',
+      modalFlag: false,
+      missId: '',
+    }
+  },
+
+  mounted() {
+    this.fetchContents()
+  },
+
+  computed: {
+    propertyList: function() {
+      return this.properties.sort(function (a, b) {
+        if(a.created_at < b.created_at) return -1
+        if(a.created_at > b.created_at) return 1
+      return 0
+    })
+    },
+  //   params() {
+  //     return {
+  //       property: {
+  //         content: this.content,
+  //         // id: this.id
+  //       }
+  //     }
+  //   }
+  },
+
+  methods: {
+    async fetchContents() {
+      const res_index = await this.$axios.get('/api/v1/properties').then((res) => {
+        this.properties = res.data.properties
+        this.selected = ""
+      })
+    },
+
+    async contentUpdate(id, $event) {
+      const url = `/api/v1/properties/${id}`
+      const res_data = await this.$axios.get(url)
+      const patch_data = res_data.data.property
+      patch_data.content = null
+      this.$axios.patch(url, patch_data, { uid: this.uid })
+        .then(res => {
+          this.fetchContents()
+        })
+        .catch((err) => {
+          const id = JSON.parse(err.config.data).id
+          this.openModal(id)
+        })
+    },
+
+    openModal(id) {
+      this.modalFlag = true
+      this.missId = id
+    },
+    closeModal(){
+      this.modalFlag = false
+      this.fetchContents()
+    }
+  }
 }
 </script>
